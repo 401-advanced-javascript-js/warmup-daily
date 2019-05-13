4fbef3cd0eaf9862ba155060ee64627c5435f826
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-05-13 15:20:10 -0700

    added comments

--- object.js
+++ object.js
@@ -1,3 +1,5 @@
+// people, stuff and state constant variables so they remain unchanged
+
 const people = ['Kookla','Fran','Ollie'];
 
 const stuff = {
@@ -17,6 +19,24 @@ const newStuff = { ...stuff, cars: [...stuff.cars, 'Honda']};
 
 let newState = {...state, people: ['Odie', ...people, 'Garfield'], stuff:{...stuff, cars: [...stuff.cars, 'Honda']}};
 
-console.log(newPeople);
+console.log(newPeople); // [ 'Odie', 'Kookla', 'Fran', 'Ollie', 'Garfield' ]
+
 console.log(newStuff);
+/* 
+  { tv: 'huge',
+    radio: 'old',
+    toothbrush: 'frayed',
+    cars: [ 'Toyota', 'Mazda', 'Honda' ] }
+ */
+
 console.log(newState);
+/*
+  { people: [ 'Odie', 'Kookla', 'Fran', 'Ollie', 'Garfield' ],
+    stuff:
+    { tv: 'huge',
+      radio: 'old',
+      toothbrush: 'frayed',
+      cars: [ 'Toyota', 'Mazda', 'Honda' ] 
+    } 
+  }
+*/
